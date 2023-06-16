cd321652319ff1c8aaaa65a23fce2c13c363181b
AnimawY <anuyitayal@gmail.com>
2023-06-16 01:30:27 -0700

    using printf in c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,15 @@
-#include<stdio.h>
-/* 
- * This program prints some english sentence
- * using the function printf in c
- * */
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ * Let's Going
+ */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
