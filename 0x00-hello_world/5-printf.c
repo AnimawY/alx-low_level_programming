5e9a159992530268cecba7f9dd4b9923413aa912
AnimawY <anuyitayal@gmail.com>
2023-06-16 00:57:58 -0700

    using printf in c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/* 
+ * This program prints some english sentence
+ * using the function printf in c
+ * */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, \n");
+return(0);
+}
