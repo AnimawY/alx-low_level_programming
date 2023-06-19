98fcc6ec9afb0261b1cf42b94287e4e51a413bd3
AnimawY <anuyitayal@gmail.com>
2023-06-18 22:37:09 -0700

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * printing one digit numbers
+ *
+ * It returns 0 - 9
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
