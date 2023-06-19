4a5e00d4c9112c23ccc8dfb057c988a944cb3264
AnimawY <anuyitayal@gmail.com>
2023-06-18 22:21:46 -0700

    print all small case letters xcepr e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* *
+ * Displaying small case characters xcept the letter q and e
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+		{
+			continue;
+		}
+		putchar(ch);
+	}
+	printf("\n");
+	return (0);
+}
